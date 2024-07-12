2f5747136bc6366fb4487be4654c6c9b6b584a35
Edmond Ina <sarerrdy4live@gmail.com>
2024-07-12 08:49:34 -0700

    auto deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ["34.73.0.174", "35.196.78.105"]
+env.hosts = ["34.239.255.39", "54.158.202.96"]
 """The list of host server IP addresses."""
 
 
