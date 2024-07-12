365c1c15f444b19a186cd9cee13147f48c300c40
Edmond Ina <sarerrdy4live@gmail.com>
2024-07-12 08:21:23 -0700

    archiever

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-"""
-a Fabric script that generates a .tgz archive
-from the contents of the web_static folder of
-AirBnB Clone repo, using the function do_pack
-"""
-from fabric.api import local
-from time import strftime
+"""A module for web application deployment with Fabric."""
+import os
+from datetime import datetime
+from fabric.api import local, runs_once
 
 
+@runs_once
 def do_pack():
-    """generaate a .tgz archive"""
-    timenow = strftime("%Y%M%d%H%M%S")
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
     try:
-        local("mkdir -p versions")
-        archname = "versions/web_static_{}.tgz".format(timenow)
-        local("tar -cvzf {} web_static/".format(archname))
-        return archname
-    except:
-        return None
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
